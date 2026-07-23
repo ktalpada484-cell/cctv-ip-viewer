76b235aa067f10ee4e4074e8cfe458b5a0433983
ktalpada484-cell <ktalpada484@gmail.com>
2026-07-23 18:20:32 +0530

    Update Log.js

--- models/Log.js
+++ models/Log.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const logSchema = new mongoose.Schema({
+  ipAddress: { type: String, required: true },
+  actionType: { type: String, required: true },
+  userAgent: { type: String },
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Log', logSchema);
